486e0bff7492babe4c0f00cc35b01d1dc427e57e
vkbinfo <vkb.info@gmail.com>
2016-07-24 16:42:11 +0530

    progress on menu item work

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.popularmovies;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +19,16 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_activity_menu,menu);
        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.popular_movies){
+            Toast.makeText(getApplicationContext(),"popular movies selected",Toast.LENGTH_LONG).show();
+        }
+        if(item.getItemId()==R.id.toprated_movies){
+            Toast.makeText(getApplicationContext(),"top rated selected",Toast.LENGTH_LONG).show();
+        }
+    return true;
     }
 }
