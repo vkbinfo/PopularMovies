ad7dbcc035715baf32db47aff0152240dc1db1d2
vkbinfo <vkb.info@gmail.com>
2016-07-24 16:22:27 +0530

    Added fragment to list movies in grid view

---
+++ app/src/main/java/com/example/android/popularmovies/MovieFragment.java
@@ -0,0 +1,25 @@
+package com.example.android.popularmovies;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by vikashkumarbijarnia on 24/07/16.
+ */
+public class MovieFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.movie_fragment_view,container,false);
+        return view;
+    }
+}
